958b340a4527cbb67362b7a0ab730ec9e3c02a86
munkhuu8 <munkhu23@yahoo.com>
2013-09-15 12:02:41 +0800

    ci add

---
+++ application/controllers/hello.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Munkh
+ * Date: 9/14/13
+ * Time: 11:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Hello extends CI_Controller {
+    public function one(){
+        echo "this one name.";
+    }
+}
